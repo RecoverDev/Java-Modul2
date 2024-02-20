4dd49ea2cf31e779a9574171e7cbc1c63aafb6fa
unknown <recover@list.ru>
2024-02-20 18:41:38 +0300

    Переименована папка Lab2 -> Lab1_3. Добавлена Лабораторная работа №2.1

---
+++ Lab2_1/src/App.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.sql.*;
+
+public class App {
+    private static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    private static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/web?user=root&password=demo";
+
+
+    public static void main(String[] args) throws Exception {
+        Class.forName(DRIVER_NAME);
+
+        String value = "";
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.print("Введите строку для поиска: ");
+            value = sc.nextLine();
+        }
+
+
+
+        try(Connection connect = DriverManager.getConnection(CONNECTION_STRING)){
+            String sql = "SELECT title, length FROM courses WHERE title LIKE '%" + value + "%' ORDER BY title";
+            Statement cmd = connect.createStatement();
+            ResultSet result = cmd.executeQuery(sql);
+
+            while(result.next()){
+                System.out.printf("%s \t %d\n", result.getString("title"),result.getInt("length"));
+            }
+
+        }
+        
+    }
+}
